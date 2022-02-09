3ebdf7d52b4209c4912e25b89cee8bbacf6e3c33
Pharap <2933055+Pharap@users.noreply.github.com>
2022-02-09 22:06:14 +0000

    Do general clean-up
    Mainly things like:
    * Adding extra space to improve readability
      * E.g. around operators
    * Removing commented-out code
    * Moving comments to the line above
    * Fixing indentation

--- cooties/cooties.ino
+++ cooties/cooties.ino
@@ -3,8 +3,8 @@
 void updateTitleScreen()
 {
   anim();
-  sprite.drawOverwrite(3,1,title_sp, title_frame);
-  sprite.drawOverwrite(14,46,push_sp,push_frame);
+  sprite.drawOverwrite(3, 1, title_sp, title_frame);
+  sprite.drawOverwrite(14, 46, push_sp, push_frame);
 }
 
 void updateGame()
@@ -21,104 +21,90 @@ void updateGame()
     --waitCount;
   }
 
-  if (hero.iframe !=0){
+  if (hero.iframe != 0){
     --hero.iframe;
   }
 
-  //draw
-  //hero
-  sprite.drawPlusMask(hero.x,hero.y,hero_sp, hero.frame);
+  // Draw hero
+  sprite.drawPlusMask(hero.x, hero.y, hero_sp, hero.frame);
 
-  //life
-  arduboy.fillRect(0,0,128,10,BLACK);
-  arduboy.fillRect(0,10,128,1,WHITE);
-  sprite.drawPlusMask(arduboy.width()-28,0,hearts_sp, hearts_frame);
-  arduboy.fillRect(arduboy.width()-28,0,hide_w,8,INVERT);
-
-  
-  //test
-  //sprite.drawPlusMask(0,0,title_sp, title_frame);
-  //sprite.drawOverwrite(0,0,push_sp,push_frame);
-  //arduboy.drawRect(targetX, targetY, targetWidth+5, targetHeight+5);
+  // Life
+  arduboy.fillRect(0, 0, 128, 10, BLACK);
+  arduboy.fillRect(0, 10, 128, 1, WHITE);
+  sprite.drawPlusMask(arduboy.width() - 28, 0, hearts_sp, hearts_frame);
+  arduboy.fillRect(arduboy.width() - 28, 0, hide_w, 8, INVERT);
 
   checkCollisions();
 
-  //points
+  // Points
   font3x5.setCursor(0, 0);
   font3x5.print("Points: ");
-  font3x5.setCursor(3*10, 0);
+  font3x5.setCursor(3 * 10, 0);
   font3x5.print(hitCount);
 
-  //level
-  font3x5.setCursor((arduboy.width()/2)-6, 0);
+  // Level
+  font3x5.setCursor((arduboy.width() / 2) - 6, 0);
   font3x5.print("Level: ");
-  font3x5.setCursor(((arduboy.width()/2)-6)+8*3, 0);
+  font3x5.setCursor(((arduboy.width() / 2) - 6) + 8 * 3, 0);
   font3x5.print(level);
 }
 
 void updateGameOver()
 {
-  //Game over screen
-  sprite.drawOverwrite(4,15,over, 0);
-  sprite.drawOverwrite(14,35,push_sp,push_frame);
+  // Game over screen
+  sprite.drawOverwrite(4, 15, over, 0);
+  sprite.drawOverwrite(14, 35, push_sp, push_frame);
 
-  arduboy.setCursor(48,52);
+  arduboy.setCursor(48, 52);
   arduboy.print("Score: ");
-  arduboy.setCursor(48+5*7,52);
-  arduboy.print(hitCount*10);
+  arduboy.setCursor(48 + 5 * 7, 52);
+  arduboy.print(hitCount * 10);
 }
 
 void setup()
 {
+  arduboy.begin();
+  arduboy.setFrameRate(60);
+  arduboy.clear();
+  arduboy.display();
+  
 
-    arduboy.begin();
-    //beep.begin();
-    arduboy.setFrameRate(60);
-    arduboy.clear();
-    arduboy.display();
-    
+  initBullets();
+  initEnemies();
 
-    initBullets();
-    initEnemies();
+  hero.x = 64;
+  hero.y = 32;
+  hero.width = 8;
+  hero.height = 8;
 
-    hero.x=64;
-    hero.y=32;
-    hero.width=8;
-    hero.height=8;
+  hitCount = 0;
 
-    hitCount=0;
+  level = 1;
 
-    level = 1;
-
-    State::Title;
-    
-    
+  State::Title;
 }
 
 void loop()
-{   //  beep.tone(beep.freq(1000),120);
-    if (!(arduboy.nextFrame())) return;
-    //beep.timer();
-    arduboy.clear();
-    input();
-    
+{
+  if (!arduboy.nextFrame())
+    return;
 
-    switch (state) {
+  arduboy.clear();
+  input();
 
-      case State::Title:
-        updateTitleScreen();
-        break;
+  switch (state) {
+    case State::Title:
+      updateTitleScreen();
+      break;
 
-      case State::Game:
-        updateGame();
-        break;
+    case State::Game:
+      updateGame();
+      break;
 
-      case State::GameOver:
-        updateGameOver();
-        break;
-    }
+    case State::GameOver:
+      updateGameOver();
+      break;
+  }
 
-    
-
-    arduboy.display();
+  arduboy.display();
 }
--- cooties/structures.h
+++ cooties/structures.h
@@ -2,74 +2,73 @@
 
 #include "general.h"
 
-
 enum class State : uint8_t
 {
-	Title, Game, GameOver
+  Title, Game, GameOver
 };
 
-State state=State::Title;
+State state = State::Title;
 
 enum class Direction : uint8_t
 {
-	Up, Right, Down, Left
+  Up, Right, Down, Left
 };
 
 struct Bullet:Rect
 {
-	Direction direction;
+  Direction direction;
 };
 
+// Size of a square bullet
+constexpr uint8_t bulletSize = 3;
 
+// Maximum number of bullets
+constexpr uint8_t bullets = 5;
 
-constexpr uint8_t bulletSize = 3; // Size of a square bullet
-constexpr uint8_t bullets = 5;    // Maximum number of bullets
-constexpr uint8_t bulletWait = 10; // Minimum frames between firing
-constexpr int bulletOff = -1;     // "Bullet not in use" value;
+// Minimum frames between firing
+constexpr uint8_t bulletWait = 10;
 
+// "Bullet not in use" value;
+constexpr int bulletOff = -1;
 
-constexpr uint8_t maxTargets = 5;    // Maximum number of target
+// Maximum number of target
+constexpr uint8_t maxTargets = 5;
 uint8_t targets = 4;
-uint8_t maxLife=3;
+uint8_t maxLife = 3;
+
 // For counting the minimum number of frames between bullets
 uint8_t waitCount = 0;
 
 Bullet bullet[bullets];
 
-
-//enemies
 // Target definitions
 constexpr uint8_t targetWidth = 8;
 constexpr uint8_t targetHeight = 8;
- uint8_t targetX = 120;
- uint8_t targetY = 30;
+uint8_t targetX = 120;
+uint8_t targetY = 30;
 
+struct Characters : Rect
+{
+  /*uint8_t w=8;
+  uint8_t h=8;
+  int16_t x=64;
+  int16_t y=32;*/
 
+  uint8_t iframe = 50;
 
-struct Characters:Rect
-{   
-    /*uint8_t w=8;
-    uint8_t h=8;
-    int16_t x=64;
-    int16_t y=32;*/
+  Direction direction;
+  bool enable = false;
 
-    uint8_t iframe=50;
+  bool collide = false;
 
-    Direction direction;
-    bool enable = false;
+  uint8_t speed = 2;
+  uint8_t frame = 0;
 
-    bool collide = false;
-
-    uint8_t speed = 2;
-    uint8_t frame = 0;
-
-    int8_t life = 3;
-
+  int8_t life = 3;
 };
 
 Characters hero;
 constexpr uint8_t heroWidth = 8;
 constexpr uint8_t heroHeight = 8;
-
 
 Characters cootie[maxTargets];
