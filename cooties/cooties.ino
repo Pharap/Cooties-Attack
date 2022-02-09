7207b1dc3c270da60a339c545ab6e496499815b8
Pharap <2933055+Pharap@users.noreply.github.com>
2022-02-09 21:48:08 +0000

    Move update code into separate functions

--- cooties/cooties.ino
+++ cooties/cooties.ino
@@ -1,5 +1,72 @@
 #include "general.h"
 
+void updateTitleScreen()
+{
+  anim();
+  sprite.drawOverwrite(3,1,title_sp, title_frame);
+  sprite.drawOverwrite(14,46,push_sp,push_frame);
+}
+
+void updateGame()
+{
+  drawCooties();
+  moveEnemy();
+  
+  moveBullets();
+  drawBullets();
+
+  setLife();
+
+  if (waitCount != 0) {
+    --waitCount;
+  }
+
+  if (hero.iframe !=0){
+    --hero.iframe;
+  }
+
+  //draw
+  //hero
+  sprite.drawPlusMask(hero.x,hero.y,hero_sp, hero.frame);
+
+  //life
+  arduboy.fillRect(0,0,128,10,BLACK);
+  arduboy.fillRect(0,10,128,1,WHITE);
+  sprite.drawPlusMask(arduboy.width()-28,0,hearts_sp, hearts_frame);
+  arduboy.fillRect(arduboy.width()-28,0,hide_w,8,INVERT);
+
+  
+  //test
+  //sprite.drawPlusMask(0,0,title_sp, title_frame);
+  //sprite.drawOverwrite(0,0,push_sp,push_frame);
+  //arduboy.drawRect(targetX, targetY, targetWidth+5, targetHeight+5);
+
+  checkCollisions();
+
+  //points
+  font3x5.setCursor(0, 0);
+  font3x5.print("Points: ");
+  font3x5.setCursor(3*10, 0);
+  font3x5.print(hitCount);
+
+  //level
+  font3x5.setCursor((arduboy.width()/2)-6, 0);
+  font3x5.print("Level: ");
+  font3x5.setCursor(((arduboy.width()/2)-6)+8*3, 0);
+  font3x5.print(level);
+}
+
+void updateGameOver()
+{
+  //Game over screen
+  sprite.drawOverwrite(4,15,over, 0);
+  sprite.drawOverwrite(14,35,push_sp,push_frame);
+
+  arduboy.setCursor(48,52);
+  arduboy.print("Score: ");
+  arduboy.setCursor(48+5*7,52);
+  arduboy.print(hitCount*10);
+}
 
 void setup()
 {
@@ -39,82 +106,15 @@ void loop()
     switch (state) {
 
       case State::Title:
-        //Title screen
-          anim();
-          sprite.drawOverwrite(3,1,title_sp, title_frame);
-          sprite.drawOverwrite(14,46,push_sp,push_frame);
-
-          
-
-          
-
+        updateTitleScreen();
         break;
 
       case State::Game:
-        //Gameplay screen
-          //input();
-
-          drawCooties();
-          moveEnemy();
-          
-          
-          moveBullets();
-          drawBullets();
-
-          setLife();
-
-          if (waitCount != 0) {
-            --waitCount;
-          }
-
-          if (hero.iframe !=0){
-            --hero.iframe;
-          }
-
-          //draw
-          //hero
-          sprite.drawPlusMask(hero.x,hero.y,hero_sp, hero.frame);
-
-          //life
-          arduboy.fillRect(0,0,128,10,BLACK);
-          arduboy.fillRect(0,10,128,1,WHITE);
-          sprite.drawPlusMask(arduboy.width()-28,0,hearts_sp, hearts_frame);
-          arduboy.fillRect(arduboy.width()-28,0,hide_w,8,INVERT);
-
-          
-          //test
-          //sprite.drawPlusMask(0,0,title_sp, title_frame);
-          //sprite.drawOverwrite(0,0,push_sp,push_frame);
-          //arduboy.drawRect(targetX, targetY, targetWidth+5, targetHeight+5);
-
-          
-
-          checkCollisions();
-
-
-          //points
-          font3x5.setCursor(0, 0);
-          font3x5.print("Points: ");
-          font3x5.setCursor(3*10, 0);
-          font3x5.print(hitCount);
-
-          //level
-          font3x5.setCursor((arduboy.width()/2)-6, 0);
-          font3x5.print("Level: ");
-          font3x5.setCursor(((arduboy.width()/2)-6)+8*3, 0);
-          font3x5.print(level);
+        updateGame();
         break;
 
       case State::GameOver:
-        //Game over screen
-        sprite.drawOverwrite(4,15,over, 0);
-        sprite.drawOverwrite(14,35,push_sp,push_frame);
-
-        arduboy.setCursor(48,52);
-        arduboy.print("Score: ");
-        arduboy.setCursor(48+5*7,52);
-        arduboy.print(hitCount*10);
-
+        updateGameOver();
         break;
     }
 
